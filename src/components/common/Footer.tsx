aedde225f30451bc278e98c2fe035decb2ca3d54
Anil Kumar Vind <akumar64587@gmail.com>
2026-06-24 18:49:13 +0530

    second commit

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -13,6 +13,7 @@ function LinkedInIcon({ className }: { className?: string }) {
   )
 }
 
+
 function YoutubeIcon({ className }: { className?: string }) {
   return (
     <svg className={className} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
